d40376d23beb038ceecb7fa136dea23ee580b9e0
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-08-06 12:51:29 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -252,7 +252,7 @@ async def help(client, message):
                 ]
             ]
         ),
-        reply_to_message_id=message.message_id
+        quote=True
     )
 
 
@@ -273,5 +273,5 @@ async def about(client, message):
                 ]                
             ]
         ),
-        reply_to_message_id=message.message_id
+        quote=True
     )
